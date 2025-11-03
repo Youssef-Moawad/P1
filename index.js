2b88a0ef478b704bb1a855bc3db80f20fc2409b0
Youssef <youssefmoawad111@gmail.com>
2025-11-03 20:25:45 +0200

    Initial commit - Timestamp Microservice

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+var expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -22,6 +25,35 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+app.get('/api/:date?', (req, res) => {
+  
+  let dateString = req.params.date;
+  let date;
+
+  
+  if (!dateString) {
+    date = new Date();
+  } 
+  
+  else if (/^-?\\d+$/.test(dateString)) {
+    date = new Date(parseInt(dateString));
+  } 
+  
+  else {
+    date = new Date(dateString);
+  }
+
+ 
+  if (date.toString() === 'Invalid Date') {
+    return res.json({ error: 'Invalid Date' });
+  }
+
+ 
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 
